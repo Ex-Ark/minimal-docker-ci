73f32fc6c425908abecee9ac0301a2b6f2b8d020
Ex-Ark <the-gameur-63@hotmail.fr>
2017-10-20 17:24:41 +0200

    modif test sh

--- test.sh
+++ test.sh
@@ -1,15 +1,9 @@
 #!/bin/sh
-set -e
-set -x
-expected="Hello World"
-actual=$(nc -v localhost 5000)
-echo "Expecting: $expected"
-echo "Server says: $actual"
-if [ "$expected" != "$actual" ]; then
-  echo "Test failed"
+if curl -4 http://localhost:5000; then
+  echo "Test passed"
   exit 1
 else
-  echo "Test passed"
+  echo "Test failed"
   exit 0
 fi
 
